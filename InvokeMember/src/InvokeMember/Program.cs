0f351d79a6bc0ae5d7749f2ca25748b5b00697ee
Henning Støverud <henning.stoverud@hr-manager.net>
2015-11-18 15:37:07 +0100

    Added project to demonstrate InvokeMember

---
+++ InvokeMember/src/InvokeMember/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime;
+
+namespace InvokeMember
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            InvokeMemberOnType(typeof (Program), new Program(),  "DoStuff", null);
+            Console.Read();
+        }
+
+        public void DoStuff()
+        {
+            Console.WriteLine("Doing stuff");
+        }
+
+        private static object InvokeMemberOnType(Type type, object target, string name, object[] args)
+        {
+            BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+            try
+            {
+                // Try to invokethe method
+                return type.InvokeMember(
+                    name,
+                    BindingFlags.InvokeMethod | bindingFlags,
+                    null,
+                    target,
+                    args);
+            }
+            catch (MissingMethodException)
+            {
+                // If we couldn't find the method, try on the base class
+                if (type.GetTypeInfo().BaseType != null)
+                {
+                    return InvokeMemberOnType(type.GetTypeInfo().BaseType, target, name, args);
+                }
+                //Don't care if the method don't exist.
+                return null;
+            }
+        }
+    }
+
+}
---
+++ InvokeMember/src/InvokeMember/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CoreCLRClassLoader")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CoreCLRClassLoader")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3494f32c-6eed-437e-ae06-29c737f99ac6")]
